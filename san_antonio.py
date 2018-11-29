9519acb8781cda1885f8c5407d25aee8d8724257
bzouleka <bzouleka@gmail.com>
2018-11-29 09:02:10 +0100

    first commit

---
+++ san_antonio.py
@@ -0,0 +1,13 @@
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+          "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
+
+# show random quote
+
+# if user_answer is "B"
+    # leave the program
+# Else :
+    # Show another quote
+
+
